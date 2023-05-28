e5d62c84d42701aed95a75ac59aad258f5e9cdb2
Swati <swatic019@gmail.com>
2023-05-29 00:13:41 +0530

    dark theme added

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ arrow.forEach((arrow) => {
   });
 });
 
-let hamburger_container = document.getElementById('hamburger_container');
-let header_hamburgerMenu = document.querySelector('.header_hamburgerMenu');
-let header_hamburgerMenu_close_contain = document.querySelector('.header_hamburgerMenu_close_contain');
+let hamburger_container = document.getElementById("hamburger_container");
+let header_hamburgerMenu = document.querySelector(".header_hamburgerMenu");
+let header_hamburgerMenu_close_contain = document.querySelector(
+  ".header_hamburgerMenu_close_contain"
+);
 
-hamburger_container.addEventListener('click', (event)=>{
+hamburger_container.addEventListener("click", (event) => {
   let toggleMenu = event.currentTarget;
   let parentDiv = toggleMenu.parentNode;
-   parentDiv.classList.toggle("toggle_hamburger_menu");
-})
+  parentDiv.classList.toggle("toggle_hamburger_menu");
+});
+
+let toggle = document.getElementById("toggle");
+toggle.addEventListener("click", () => {
+  document.getElementsByTagName("body")[0].classList.toggle("dark_theme");
+});
